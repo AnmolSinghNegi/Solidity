cc1b0569c84b87d4c9be3617c53434c5a0433c2e
AnmolSinghNegi <99644009+AnmolSinghNegi@users.noreply.github.com>
2023-08-25 23:53:29 +0530

    Add files via upload

---
+++ Basis/LocalVariable.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract LocalVar {
+    function getLocal() public pure returns (uint) {
+        uint rollNumber;
+        rollNumber=10;
+        return rollNumber;
+    }
+}
